a277b86cf0f169b3ff72121575bf265bcf2de064
Jocelyn Chang <jrc300@gmail.com>
2017-08-01 23:33:17 -0700

    started changing db from MySQL to Redis

--- app.py
+++ app.py
@@ -1,103 +1,47 @@
 
 from flask import Flask, render_template, url_for, request
 from flask import g, Markup
-from flask.ext.sqlalchemy import SQLAlchemy
+from redis import Redis
 from Levenshtein import distance
 from operator import itemgetter
 import random
 
-class MyServer(Flask):
-
-    def __init__(self, *args, **kwargs):
-            super(MyServer, self).__init__(*args, **kwargs)
-
-            #instanciate your variables here
-            self.computer_tscrpt = []
-            self.user_tscrpt = []
-
 app = MyServer(__name__)
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://centralbureacracy:centralfiling@localhost/convo'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://train:kaggle@localhost/convo'
-db = SQLAlchemy(app)
+r = Redis(host='localhost', port=6379)
 
-'''
-updated to many-to-many relationship
-'''
-node_to_node = db.Table("node_to_node", 
-    db.Column("statement_id", db.Integer, db.ForeignKey("node.id"), primary_key=True),
-    db.Column("response_id", db.Integer, db.ForeignKey("node.id"), primary_key=True),
-    db.Column("weight", db.Float)
-)
-
-class Node(db.Model):
-	__tablename__ = 'node'
-	id = db.Column(db.Integer, primary_key=True)
-	text = db.Column(db.String(500))
-	freq = db.Column(db.Float)
-	response_nodes = db.relationship("Node",
-						secondary=node_to_node,
-						primaryjoin=id==node_to_node.c.statement_id,
-						secondaryjoin=id==node_to_node.c.response_id,
-						backref="statement_nodes")
 
 '''
 use Levenshtein distance to find the text most similar to the text the user
 just imputted
 '''
-def closest(words, lst):
-    return min([(str(x.id), distance(words, x.text)) for x in lst], key=itemgetter(1))
+def closest(inquiry, keys):
+  return min([(str(x.id), distance(inquiry, x.text)) for x in keys], key=itemgetter(1))
 
 '''
 get and process a response from the sqlalchemy db
 input text does not need to be already in the db
 '''
-def get_comp_response(words):
-	s=Node.query.all()
-	c=closest(words, s)
-	n=Node.query.filter(Node.id==int(c[0])).first()
-	return select_response(n.response_nodes)
+def get_comp_response(inquiry):
+	keys = r.scan_iter(match='*')
+	closest_phrase=closest(inquiry, keys)
+	return r.zcard(closest_phrase)
 
-'''
-Select a response from the different response nodes.
-The normalized frequency of the response is added to a random factor to create a weight.
-The max weight is selected as the response.
-'''
-def select_response(response_nodes):
-	freqs = []
-	weights=[]
-	for r in response_nodes:
-		freqs.append(r.freq)
-	total = sum(freqs)
-	for f in freqs:
-		f=float(f/total)
-	for f in freqs:
-		weights.append(f+random.random())
-	max_index = weights.index(max(weights))
-	response_nodes[max_index].freq+=1
-	db.session.commit()
-	return response_nodes[max_index]
 
-def add_db_user_text(words, previous_words):
+def add_db_user_text(inquiry, user_response):
 	#check if user input exists
-	ur=Node.query.filter(Node.text.ilike(words)).first()
-	if ur:
-		#check if there is a connection between user input and statement
-		ps=Node.query.filter(Node.text.ilike(previous_words)).first()
-		if ur.id!=ps.response_nodes[0].id:
-			ps.response_nodes.append(ur)
-			db.session.commit()
+	elements = r.zscan_iter(inquiry, match=user_response):
+	if elements:
+		keys = r.scan_iter(match='*')
+		for k in keys:
+			if k==user_response:
+				r.zincby(inquiry, user_response)
+				return
+		r.zadd(inquiry, 1, user_response)
 	else:
-		#add user input to Node table
-		r=Node(text=words, freq=0)
-		db.session.add(r)
-		db.session.commit()
-		#link user input to response and parent statement
-		ps=Node.query.filter(Node.text.ilike(previous_words)).first()
-		ps.response_nodes.append(r)
-		db.session.add(ps)
-		db.session.commit()
-	return
+		#add user input db
+		r.zadd(inquiry, 1, user_response)
+		return
 
 #main app
 @app.route('/', methods=['POST', 'GET'])
@@ -134,5 +78,4 @@ def index ():
 
 
 if __name__ == '__main__':
-	app.debug=True
-	app.run(port=5000)
+   app.run(host="0.0.0.0", debug=True)
